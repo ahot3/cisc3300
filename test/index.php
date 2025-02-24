be9d635892cc0b2a4261e57323ebd3518b5e7d5c
ahot3 <ahot3@fordham.edu>
2025-02-24 17:18:33 -0500

    complete

--- test/index.php
+++ test/index.php
@@ -6,12 +6,12 @@ if ($page == "html") {
     echo "<!DOCTYPE html>
     <html lang='en'>
     <head><title>HTML Page</title></head>
-    <body><h1>This is an HTML response!</h1></body>
+    <body><h1>This is the HTML response</h1></body>
     </html>";
 }
 elseif ($page == "json") {
     header("Content-Type: application/json");
-    $response = ["message" => "This is a JSON response!"];
+    $response = ["message" => "This is the JSON response"];
     echo json_encode($response);
 }
 else {
