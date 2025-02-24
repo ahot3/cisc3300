1a14e4ccbb4105526dc35948a649ad4fc6b4b7e8
ahot3 <ahot3@fordham.edu>
2025-02-24 16:50:12 -0500

    update

--- in-class-12/response.php
+++ in-class-12/response.php
@@ -1,21 +1,17 @@
 <?php
 $request = $_SERVER['REQUEST_URI'];
 
-if ($request == "/html") {
+if ($request == "/test/html") {
     header("Content-Type: text/html");
     echo "<!DOCTYPE html>
     <html lang='en'>
-    <head>
-        <title>HTML Page</title>
-    </head>
-    <body>
-        <h1>HTML Response</h1>
-    </body>
+    <head><title>HTML Page</title></head>
+    <body><h1>This is the HTML response.</h1></body>
     </html>";
 }
-elseif ($request == "/json") {
+elseif ($request == "/test/json") {
     header("Content-Type: application/json");
-    $response = ["message" => "JSON Response"];
+    $response = ["message" => "This is the JSON response."];
     echo json_encode($response);
 }
 else {
