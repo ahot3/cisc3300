ce9d9201855fbdd33d233bc884ae03ad7da821d0
ahot3 <ahot3@fordham.edu>
2025-02-24 16:56:50 -0500

    d

--- in-class-12/index.php
+++ in-class-12/index.php
@@ -1,17 +1,19 @@
 <?php
+// Debugging
 $request = $_SERVER['REQUEST_URI'];
+echo "<p>Debug: Request URI is <strong>$request</strong></p>";
 
 if ($request == "/test/html") {
     header("Content-Type: text/html");
     echo "<!DOCTYPE html>
     <html lang='en'>
     <head><title>HTML Page</title></head>
-    <body><h1>This is the HTML response.</h1></body>
+    <body><h1>This is an HTML response!</h1></body>
     </html>";
 }
 elseif ($request == "/test/json") {
     header("Content-Type: application/json");
-    $response = ["message" => "This is the JSON response."];
+    $response = ["message" => "This is a JSON response!"];
     echo json_encode($response);
 }
 else {
