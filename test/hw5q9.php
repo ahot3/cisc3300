6e1725a4f22aab17a9541e63f87886f9c2811677
ahot3 <ahot3@fordham.edu>
2025-02-27 23:27:57 -0500

    hw5

--- test/hw5q9.php
+++ test/hw5q9.php
@@ -1,10 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
 
 $uri = strtok($_SERVER["REQUEST_URI"], '?');
-$uriArray = explode("/", $uri);
+$uriArray = explode("/", trim($uri, "/"));
 
-if ($uriArray[1] === 'hw5q9' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+if (isset($uriArray[1]) && $uriArray[1] === 'hw5q9' && $_SERVER['REQUEST_METHOD'] === 'GET') {
     $fruits = [
         ['name' => 'Apple', 'price' => 1.50],
         ['name' => 'Banana', 'price' => 0.75],
@@ -17,6 +18,6 @@ if ($uriArray[1] === 'hw5q9' && $_SERVER['REQUEST_METHOD'] === 'GET') {
     exit();
 }
 
-echo json_encode(['error' => 'Invalid request']);
+echo json_encode(["error" => "Invalid request"]);
 exit();
 ?>
