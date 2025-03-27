bdbe917985faa63c55ca220f154cabb772e98226
ahot3 <ahot3@fordham.edu>
2025-03-27 16:31:48 -0400

    icl19

---
+++ in-class-19/index.php
@@ -0,0 +1,17 @@
+<?php
+require 'db.php';
+
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($request === '/posts') {
+    $stmt = $pdo->prepare("SELECT * FROM posts");
+    $stmt->execute();
+    $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    header('Content-Type: application/json');
+    echo json_encode($posts);
+    exit;
+}
+
+http_response_code(404);
+echo json_encode(['error' => 'Not Found']);
